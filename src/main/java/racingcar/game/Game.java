10927b51f3b61556aad649f9e04844e659af16c1
kokeunho <kokangsik65@naver.com>
2025-10-26 00:01:13 +0900

    feat(game): 게임 세팅, 라운드 진행 및 현황 출력, 우승자 반환 메서드 작성

---
+++ src/main/java/racingcar/game/Game.java
@@ -0,0 +1,68 @@
+package racingcar.game;
+
+import racingcar.car.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+
+    private final List<Car> carList;
+    private final int tryCount;
+
+    public Game(String carNames, int tryCount) {
+        this.carList = createCarList(carNames);
+        this.tryCount = tryCount;
+    }
+
+    private List<Car> createCarList(String carNames) {
+        List<Car> carList = new ArrayList<>();
+
+        for (String name : carNames.split(",")) {
+            carList.add(new Car(name.trim()));
+        }
+
+        return carList;
+    }
+
+    private void playRound() {
+        for (Car car : carList) {
+            car.moveCar();
+        }
+    }
+
+    private void printRoundResult() {
+        for (Car car : carList) {
+            String carPosition = "-".repeat(car.getPosition());
+            System.out.println(car.getName() + " : " + carPosition);
+        }
+        System.out.println();
+    }
+
+    public void start() {
+        System.out.println("\n실행 결과");
+
+        for (int i = 0; i < tryCount; i++) {
+            playRound();
+            printRoundResult();
+        }
+    }
+
+    public List<String> getWinners() {
+        int maxPosition = 0;
+
+        for (Car car : carList) {
+            maxPosition = Math.max(maxPosition, car.getPosition());
+        }
+
+        List<String> winners = new ArrayList<>();
+
+        for (Car car : carList) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car.getName());
+            }
+        }
+
+        return winners;
+    }
+}
