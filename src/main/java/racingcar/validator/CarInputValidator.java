1885a9fd5c746d3aac6df7b1d6130ea04b1b22a5
kokeunho <kokangsik65@naver.com>
2025-10-25 15:23:03 +0900

    feat(validator): 차량 입력 검증

---
+++ src/main/java/racingcar/validator/CarInputValidator.java
@@ -0,0 +1,22 @@
+package racingcar.validator;
+
+public class CarInputValidator implements Validator<String> {
+
+    @Override
+    public void validate(String input) {
+
+        String[] cars = input.split(",");
+
+        // 1. 입력된 차량의 수가 2대 미만이면 예외
+        if (cars.length < 2) {
+            throw new IllegalArgumentException("차량은 2대 이상이어야 합니다.");
+        }
+
+        // 2. 입력된 차량명 중 1~5글자를 벗어나면 예외
+        for (String car : cars) {
+            if (car.isEmpty() || car.length() > 5) {
+                throw new IllegalArgumentException("차량명은 최소 1글자 최대 5글자여야 합니다.");
+            }
+        }
+    }
+}
---
+++ src/test/java/racingcar/validator/CarInputValidatorTest.java
@@ -0,0 +1,61 @@
+package racingcar.validator;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarInputValidatorTest {
+
+    private CarInputValidator carInputValidator;
+
+    @BeforeEach
+    void setUp() {
+        carInputValidator = new CarInputValidator();
+    }
+
+    @Test
+    void 정상_입력() {
+
+        //when
+        String input = "car1,car2,car3,car4";
+
+        //then
+        assertDoesNotThrow(() -> carInputValidator.validate(input));
+    }
+
+    @Test
+    void 차량수_조건() {
+
+        //when
+        String input1 = "";
+        String input2 = "car1";
+
+        //then
+        assertThatThrownBy(() -> carInputValidator.validate(input1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("차량은 2대 이상이어야 합니다.");
+        assertThatThrownBy(() -> carInputValidator.validate(input2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("차량은 2대 이상이어야 합니다.");
+    }
+
+    // 글자수가 1~5글자여야 함
+    @Test
+    void 글자수_조건() {
+
+        //when
+        String input1 = "car1,,car3,car4";
+        String input2 = "carcar1,car2,car3,car4";
+
+        //then
+        assertThatThrownBy(() -> carInputValidator.validate(input1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("차량명은 최소 1글자 최대 5글자여야 합니다.");
+        assertThatThrownBy(() -> carInputValidator.validate(input2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("차량명은 최소 1글자 최대 5글자여야 합니다.");
+    }
+}
