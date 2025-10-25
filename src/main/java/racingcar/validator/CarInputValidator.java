f3b3eaa7e2346ff3aaa4ccf12f502726696bbb47
kokeunho <kokangsik65@naver.com>
2025-10-26 00:06:37 +0900

    fix(validator): 입력 문자열에서 공백 문자 처리 추가

--- src/main/java/racingcar/validator/CarInputValidator.java
+++ src/main/java/racingcar/validator/CarInputValidator.java
@@ -5,7 +5,7 @@ public class CarInputValidator implements Validator<String> {
     @Override
     public void validate(String input) {
 
-        String[] cars = input.split(",");
+        String[] cars = input.trim().split(",");
 
         // 1. 입력된 차량의 수가 2대 미만이면 예외
         if (cars.length < 2) {
@@ -14,6 +14,8 @@ public class CarInputValidator implements Validator<String> {
 
         // 2. 입력된 차량명 중 1~5글자를 벗어나면 예외
         for (String car : cars) {
+            car = car.trim();
+
             if (car.isEmpty() || car.length() > 5) {
                 throw new IllegalArgumentException("차량명은 최소 1글자 최대 5글자여야 합니다.");
             }
