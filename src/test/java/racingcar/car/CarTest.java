7040adeb0f7e44d6d5eca4eab2e997d551d3e563
kokeunho <kokangsik65@naver.com>
2025-10-26 19:52:41 +0900

    test(car): assertRandomNumberInRangeTest 활용하여 비결정적 메서드 move 테스트

--- src/test/java/racingcar/car/CarTest.java
+++ src/test/java/racingcar/car/CarTest.java
@@ -2,34 +2,42 @@ package racingcar.car;
 
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
 
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
     @Test
     void 차량_전진() {
-
         // given
         Car car = new Car("car1");
 
-        // when
-        car.moveCarTest(4);
-
-        // then
-        assertThat(car.getPosition()).isEqualTo(1);
+        // when & then
+        assertRandomNumberInRangeTest(
+                () -> {
+                    car.move();
+                    assertThat(car.getPosition()).isEqualTo(1);
+                },
+                MOVING_FORWARD
+        );
     }
 
     @Test
     void 차량_정지() {
-
         // given
         Car car = new Car("car1");
 
-        // when
-        car.moveCarTest(3);
-
-        // then
-        assertThat(car.getPosition()).isEqualTo(0);
+        // when & then
+        assertRandomNumberInRangeTest(
+                () -> {
+                    car.move();
+                    assertThat(car.getPosition()).isEqualTo(0);
+                },
+                STOP
+        );
     }
 
 }
